5a6803cbdd5bce1d9ed31e9d0ff16d1b53c1af1c
berbarh681 <labeebahadeoti@gmail.com>
2022-04-16 00:12:04 -0800

    jsfiles added

---
+++ code.js
@@ -0,0 +1,4 @@
+//comments can make code readable//
+//welcome to 30daysofjavascript//
+/*comments can make code readable,
+easy to reuse and informative*/
---
+++ variable.js
@@ -0,0 +1,67 @@
+/*create a variable.js file and declare variables
+ and assign string,boolean,undefined and null data types*/
+let items = "book";
+let single = true;
+let religion 
+let jobExperience = null;
+
+
+//create a datatypes.js filw
+let datatype1 = "false"
+let datatype2 = 6.7;
+let datatype3 =  true
+let datatype4 
+let datatype5 = null
+
+//longing datatype on the console
+console.log(typeof items);
+console.log(typeof single);
+console.log(typeof religion);
+console.log(typeof jobExperience);
+
+//declare four variables without assigning values
+let friends
+let school
+let family
+let club
+
+//declare four variables with assigned values
+let variable1 = "key"
+let variable2 
+let variable3 = 5
+let variable4  = true
+
+/*declare variables to store your first name,
+ last name, marital status, country
+ and age in multiple lines*/
+ let firstName ="labeebah"
+ let last_name = adeoti
+ let married = true
+ let $country ="united kingdom"
+
+ /*declare variables to store your first name,
+ last name, marital status, country
+ and age in a single line*/
+ let( firstName ="labeebah", last_name = adeoti , married = true , $country = "united kingdom")
+
+ /*write a single line comment which says, 
+ comments can make code readable*/
+ // comments can make code readable
+
+ /*write another single line comment which says, 
+ welcome to 30Days of JavaScript*/
+ //Welcome to 30Days of JavaScript
+
+ /*write a multiline comment which says,
+ comments can make code readable,
+ easy to reuse and informative*/
+ /* comments can make code readable,
+ easy to reuse and informative*/
+ 
+ /*declare two variables myAge and yourAge and assign
+ them initial values and log to the browser console*/
+ let myAge = 25
+ let yourAge = 30
+ 
+ 
+ 
